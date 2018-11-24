5ac0de203a44871711cf26ba95b4c4985dc23816
pnochisaki <pnochisaki@gmail.com>
2018-11-24 02:06:14 -0800

    ecomm

--- src/assets/scripts/init.js
+++ src/assets/scripts/init.js
@@ -1,7 +1,57 @@
+
 $(function () {
 
-    $('.products form').each(function(){
-        $(this).find('script').attr('data-amount', 2000)
+    var handler = StripeCheckout.configure({
+        key: 'pk_live_aqqrWSGqwzp5AH0IkoKypeb2',
+        image: '/assets/img/star-white.jpg',
+        locale: 'auto',
+        token: function (token) {
+            // You can access the token ID with `token.id`.
+            // Get the token ID to your server-side code for use.
+        }
     });
+    var description = "Vintage Origami Sheet Music Christmas Stars"
+    var qty = 1
+    var amount = 300
+    var total = qty * amount
+
+    $('#price').text(total/100);
+    
+    $('input[type="number"]').on('change', function () {
+        var qty = $(this).val();
+
+        if (qty > 1) {
+            amount = 250
+            description = qty + ' Christmas Stars'
+        } else {
+            amount = 300
+        }
+
+        howmany = qty;
+        total = qty * amount;
+        $('#price').text(total/100);
+
+        document.getElementById('customButton').addEventListener('click', function (e) {
+
+            // Open Checkout with further options:
+            handler.open({
+                name: 'Oxigami',
+                description: description,
+                amount: total
+            });
+            e.preventDefault();
+        });
+
+        // Close Checkout on page navigation:
+        window.addEventListener('popstate', function () {
+            handler.close();
+            howmany = 1;
+            $('#price').text(total/100);
+        });
+
+    });
+
+    $('input[type="number"]').trigger("change");
+
 
 });
--- src/index.md
+++ src/index.md
@@ -3,3 +3,4 @@ title: Home
 layout: home.hbs
 body_classes: home
 ---
+My name is Oscar, I’m 7 1/2 years old and I really like making origami. These Christmas Stars are special because they’re made with sheet music that my grandma, my great grandma, and my great-great grandmas all used to teach piano. I’m selling them as Gift Toppers and Christmas Tree Ornaments for $3 each or 2/$5. I will hand deliver to your front door in my neighborhood or I can mail them to you for a small extra price. You can message my mom on here or email Ox@Oxigami.com to make an order. Thank you! Happy Holidays! 
